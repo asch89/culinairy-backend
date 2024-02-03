409822962b33643b4c192c6d0962f0a90cbf6167
Joey Aschenbrenner <j10612a@gmail.com>
2024-02-03 13:59:43 -0500

    removed commented code

--- index.js
+++ index.js
@@ -20,14 +20,6 @@ app.use(Express.json());
 
 app.post('/', async (req, res) => {
     let data = req.body.join(", ");
-    console.log(data)
-    // const completion = await openai.createCompletion({
-    //     model: "text-babbage-001",
-    //     prompt: `give me some recipe ideas for dinner using the following ingredients: ${data}`,
-    //     temperature: 0.6,
-    //     max_tokens: 1024,
-    // });
-    // res.status(200).json({ result: completion.data.choices[0].text });
 
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
